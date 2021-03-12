5f66274524e48c764f2946eda11bb5405d2d660e
Harikesh Pallantla <p.harikesh409@gmail.com>
2021-03-12 17:28:12 +0530

    Added entities.

---
+++ backend/cred/src/main/java/com/crio/cred/entity/Category.java
@@ -0,0 +1,23 @@
+package com.crio.cred.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "category")
+public class Category extends BaseEntity {
+    @Id
+    @GeneratedValue
+    private Long categoryId;
+
+    @Column(nullable = false, unique = true)
+    private String category;
+}
---
+++ backend/cred/src/main/java/com/crio/cred/entity/Vendor.java
@@ -0,0 +1,23 @@
+package com.crio.cred.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "vendor")
+public class Vendor extends BaseEntity {
+    @Id
+    @GeneratedValue
+    private Long vendorId;
+
+    @Column(nullable = false, unique = true)
+    private String vendor;
+}
