3e290b1bfbf95d844cd36eb9fcb0d9c00a2a18ef
Harikesh <p.harikesh409@gmail.com>
2021-03-28 12:20:36 +0530

    Added token for getcards

--- frontend/cred/src/services/card-service.tsx
+++ frontend/cred/src/services/card-service.tsx
@@ -3,24 +3,24 @@ import { USER_TOKEN } from '../constants/store-constants'
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL
 
-export function getAllCardsApi(success:(response:any) => any, error:(error:any) => any) {
-    axios.get(baseUrl + "cards")
-        .then(res => success(res))
-        .catch(err => error(err))
+export function getAllCardsApi(success: (response: any) => any, error: (error: any) => any) {
+  axios.get(baseUrl + "cards", getConfig())
+    .then(res => success(res))
+    .catch(err => error(err))
 }
 
-export function addCardApi(payload: any, success:(response:any) => any, error:(error:any) => any) {
-    axios.post(baseUrl + "cards", payload, getConfig())
-        .then(res => success(res))
-        .catch(err => error(err))
+export function addCardApi(payload: any, success: (response: any) => any, error: (error: any) => any) {
+  axios.post(baseUrl + "cards", payload, getConfig())
+    .then(res => success(res))
+    .catch(err => error(err))
 }
 
-export function getStatementApi(id: number, year: number, month: number, 
-  success:(response:any) => any, error:(error:any) => any) {
-    axios.get(baseUrl + "cards/"+id+"/statements/"+year+"/"+month, getConfig())
-        .then(res => success(res))
-        .catch(err => error(err))
-  }
+export function getStatementApi(id: number, year: number, month: number,
+  success: (response: any) => any, error: (error: any) => any) {
+  axios.get(baseUrl + "cards/" + id + "/statements/" + year + "/" + month, getConfig())
+    .then(res => success(res))
+    .catch(err => error(err))
+}
 
 function getConfig() {
   return { headers: { 'Authorization': 'Bearer ' + localStorage.getItem(USER_TOKEN) } }
